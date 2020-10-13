04a29a89b4e2230146fc54cbdef110c7e21a8e84
Kevin Dees <kvdees@gmail.com>
2020-10-12 22:38:45 -0400

    cleanup auto loader

--- typerocket/vendor/composer/platform_check.php
+++ typerocket/vendor/composer/platform_check.php
@@ -9,15 +9,7 @@ if (!(PHP_VERSION_ID >= 70205)) {
 }
 
 $missingExtensions = array();
-extension_loaded('dom') || $missingExtensions[] = 'dom';
-extension_loaded('filter') || $missingExtensions[] = 'filter';
 extension_loaded('json') || $missingExtensions[] = 'json';
-extension_loaded('libxml') || $missingExtensions[] = 'libxml';
-extension_loaded('mbstring') || $missingExtensions[] = 'mbstring';
-extension_loaded('phar') || $missingExtensions[] = 'phar';
-extension_loaded('tokenizer') || $missingExtensions[] = 'tokenizer';
-extension_loaded('xml') || $missingExtensions[] = 'xml';
-extension_loaded('xmlwriter') || $missingExtensions[] = 'xmlwriter';
 
 if ($missingExtensions) {
     $issues[] = 'Your Composer dependencies require the following PHP extensions to be installed: ' . implode(', ', $missingExtensions);
